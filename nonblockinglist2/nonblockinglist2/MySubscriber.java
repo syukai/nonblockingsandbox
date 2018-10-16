726b48ad7ff7e7e16c9f90c5e7c863db3f410ef4
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-10-17 06:50:16 +0900

    single thread subscribe

--- nonblockinglist2/nonblockinglist2/MySubscriber.java
+++ nonblockinglist2/nonblockinglist2/MySubscriber.java
@@ -7,34 +7,22 @@ public class MySubscriber<T> implements Subscriber<T> {
 	
 	private Subscription subscription;
 	private final String name;
-	private final int interval;
 	
-	public MySubscriber(String name, int interval) {
+	public MySubscriber(String name) {
 		this.name = name;
-		this.interval = interval;
 	}
 
 	@Override
 	public void onSubscribe(Subscription subscription) {
 		this.subscription = subscription;
+		System.out.println(name + ": " + Thread.currentThread().getName() + " onSubscribe " );
 		this.subscription.request(1);
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
 	}
 
 	@Override
 	public void onNext(T item) {
-		System.out.println(name + ": " + Thread.currentThread().getName() + ": " + item + ", interval: " + this.interval);
-		try {
-			Thread.sleep(this.interval);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println(name + ": " + Thread.currentThread().getName() + ": " + item );
 		subscription.request(1);
 	}
 
