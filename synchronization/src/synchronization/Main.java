e559ca334a33b9c6e9d6adbba2fd7a2680514d64
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-11-01 23:23:48 +0900

    add syncrhnization init

---
+++ synchronization/src/synchronization/Main.java
@@ -0,0 +1,31 @@
+package synchronization;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+public class Main {
+	
+	private static class Calc{
+	    private int sum = 0;
+	    private int getSum() {return this.sum;}
+	    private void setSum(int value) {this.sum = value;}
+	    
+	    public void calculate() {
+	        setSum(getSum() + 1);
+	    }
+	}
+
+	public static void main(String[] argv) throws InterruptedException {
+	    ExecutorService service = Executors.newFixedThreadPool(3);
+	    Calc summation = new Calc();
+	 
+	    IntStream.range(0, 1000)
+	      .forEach(count -> service.submit(summation::calculate));
+	    service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+	 
+	    // 1000にならない！！！
+	    System.out.println(summation.getSum());
+	}
+}
