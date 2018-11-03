232a04e62780465781d7e118c437e3086989e282
syukai <d_hirayama@rapid.ocn.ne.jp>
2018-11-03 10:32:12 +0900

    synchronize static method

--- synchronization/src/synchronization/Main.java
+++ synchronization/src/synchronization/Main.java
@@ -8,6 +8,7 @@ import java.util.stream.IntStream;
 public class Main {
 	
 	private static class Calc{
+		private static int staticSum = 0;
 	    private int sum = 0;
 	    private int getSum() {return this.sum;}
 	    private void setSum(int value) {this.sum = value;}
@@ -20,6 +21,22 @@ public class Main {
 	    }
 	    public synchronized void syncDirectCalculate() {
 	    	sum++;
+	    }
+	    public void syncblockDirectCalculate() {
+	    	synchronized(this) {
+	    		sum++;
+	    	}
+	    }
+	    
+	    public static int getStaticSum() {return staticSum;}
+	    public static void resetStaticSum() {staticSum = 0;}
+	    public static void staticDirectCalculate() {
+	    	staticSum++;
+	    }
+	    public static void staticSyncblockDirectCalculate() {
+	    	synchronized(Calc.class) {
+	    		staticSum++;
+	    	}
 	    }
 	}
 
@@ -50,6 +67,28 @@ public class Main {
 	    // 1000になる！！！
 	    System.out.println(summation.getSum());
 	    
+	    summation.setSum(0);
+
+	    IntStream.range(0, 1000)
+	    	.forEach(count -> service.submit(summation::syncblockDirectCalculate));
+	    service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+	    // 1000になる！！！
+	    System.out.println(summation.getSum());
+	    
+	    
+	    IntStream.range(0, 1000)
+	    	.forEach(c -> service.submit(Calc::staticDirectCalculate));
+	    service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+	    // 1000にならない
+	    System.out.println(Calc.getStaticSum());
+	    
+	    Calc.resetStaticSum();
+	    
+	    IntStream.range(0, 1000)
+	    	.forEach(c -> service.submit(Calc::staticSyncblockDirectCalculate));
+	    service.awaitTermination(1000, TimeUnit.MILLISECONDS);
+	    // 1000になる！！！
+	    System.out.println(Calc.getStaticSum());
 	    service.shutdown();
 	}
 }
