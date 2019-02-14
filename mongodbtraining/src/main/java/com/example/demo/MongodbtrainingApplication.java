5e5c1fc02d095b11af0b45647aa5ddb58255b646
syukai <d_hirayama@rapid.ocn.ne.jp>
2019-02-15 07:08:05 +0900

    mongodb init

---
+++ mongodbtraining/src/main/java/com/example/demo/MongodbtrainingApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbtrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbtrainingApplication.class, args);
+	}
+
+}
+
